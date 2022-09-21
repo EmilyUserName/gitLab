80e8ac0da139359998b13848d8cbe5e90d2fc67c
Emily Davidson <emilyabigaildavidson@gmail.com>
2022-09-21 11:43:10 -0400

    first commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  let Lat = 38.8977;
+  let Long = 77.0365;
+
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  );
+}
+export default App;
